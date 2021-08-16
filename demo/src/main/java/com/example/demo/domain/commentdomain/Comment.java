389a1f494ddbbff9b8fef5dbe1b2d4bc79b0f3a5
carloscastillogarcia <krestinho@gmail.com>
2021-08-16 11:03:18 +0200

    Comment y User

--- demo/src/main/java/com/example/demo/domain/commentdomain/Comment.java
+++ demo/src/main/java/com/example/demo/domain/commentdomain/Comment.java
@@ -1,5 +1,28 @@
 package com.example.demo.domain.commentdomain;
 
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Type;
+
+@Entity
 public class Comment {
-    
+    @Id
+    @Type (type = "uuid-char")
+    public UUID id;
+    @Column (nullable = false)
+    public String text;
+    @Column (nullable = false)
+    public float rating;
+    @Column (nullable = false)
+    public String date;
+    @Column (nullable = false)
+    @Type (type = "uuid-char")
+    public UUID user;
+    @Column (nullable = false)
+    @Type (type = "uuid-char")
+    public UUID pizza;
 }
--- demo/src/main/java/com/example/demo/domain/userdomain/User.java
+++ demo/src/main/java/com/example/demo/domain/userdomain/User.java
@@ -1,5 +1,26 @@
 package com.example.demo.domain.userdomain;
 
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Type;
+
+@Entity
 public class User {
-    
+    @Id
+    @Type (type = "uuid-char")
+    public UUID id;
+    @Column (nullable = false)
+    public String name;
+    @Column (nullable = false)
+    public String lastName;
+    @Column (nullable = false, unique = true)
+    public String email;
+    @Column (nullable = false)
+    public String password;
+
+
 }
