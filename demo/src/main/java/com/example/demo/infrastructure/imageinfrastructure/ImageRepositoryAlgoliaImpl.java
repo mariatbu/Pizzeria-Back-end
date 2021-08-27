618781ee0bba72ea8fd9c51dec104d0c862e0990
carloscastillogarcia <krestinho@gmail.com>
2021-08-27 14:36:00 +0200

    Image v2

--- demo/src/main/java/com/example/demo/infrastructure/imageinfrastructure/ImageRepositoryAlgoliaImpl.java
+++ demo/src/main/java/com/example/demo/infrastructure/imageinfrastructure/ImageRepositoryAlgoliaImpl.java
@@ -2,11 +2,11 @@ package com.example.demo.infrastructure.imageinfrastructure;
 
 import com.example.demo.domain.userdomain.image.Image;
 import com.example.demo.domain.userdomain.image.ImageRepositoryAlgolia;
-import com.example.demo.domain.userdomain.image.imageAlgolia;
+import com.example.demo.domain.userdomain.image.ImageAlgolia;
 
 public class ImageRepositoryAlgoliaImpl implements ImageRepositoryAlgolia {
     @Override
-    public imageAlgolia add(Image image) {
-        return new imageAlgolia();
+    public ImageAlgolia add(Image image) {
+        return new ImageAlgolia();
     }
 }
